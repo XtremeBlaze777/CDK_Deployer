53adc887e2727d350a6c7e3a85f27d3995ddbff8
XtremeBlaze777 <ratikmathur@gmail.com>
2022-08-18 23:56:29 -0400

    Set bash script to use unix line endings (lf) and pwsh to dos (cr lf); also collapsed all cdk deploys into one command for pwsh

--- deploy.ps1
+++ deploy.ps1
@@ -3,26 +3,28 @@ cdk bootstrap
 
 $STACKNUM = Read-Host "How many stacks will you be deploying? (enter 0 if none)"
 $STACKLIST = @()
+$COMMAND = "cdk deploy"
 
 for ($i = 1; $i -le $STACKNUM; $i++) {
 	$TMP = Read-Host "Name of Stack $i"
 	$STACKLIST += @( $TMP )
+	$COMMAND += " $TMP"
 }
 
 foreach ($STACK in $STACKLIST) {
 	Start-Sleep -Seconds 1
 	$PARAMS = Read-Host "How many parameters does $STACK take? (Enter 0 if none)"
 	
-	$COMMAND = "cdk deploy $STACK"
 	if ( $PARAMS -gt 0 ) {
 		for ($PARAM = 1; $PARAM -le $PARAMS; $PARAM++) {
 			$NAME = Read-Host "Enter parameter $PARAM's name (not the value) listed in your CDK code"
 			$VALUE = Read-Host "Enter $NAME's value"
 
-			$COMMAND+=" --parameters $NAME=$VALUE"
+			$COMMAND+=" --parameters $STACK`:$NAME=$VALUE"
 		}
 	}
 	Write-Output "Deploying $STACK"
 	Start-Sleep -Seconds 2
-	Invoke-Expression $COMMAND
 }
+
+Invoke-Expression $COMMAND
